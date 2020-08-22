6a9bf066702ae2edb88b8a0e9d86ce0bb13eb073
dsab123 <dsabbaghumd@gmail.com>
2020-08-22 18:35:59 -0400

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client } = require('pg');
+    
+const username = process.env.Username;
+const password = process.env.Password;
+const server = process.env.Server;
+const database = process.env.Database;
+
+exports.handler = async (event) => {
+    const client = new Client({
+      user: username,
+      host: server,
+      database: database,
+      password: password,
+      port: 5432,
+    });
+    
+    client.connect();
+
+    let result = await client.query('select * from booksummaryinfo');
+    
+    if (result.rows.length > 0) {    
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.rows),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify([]),
+      };
+    }
+};
